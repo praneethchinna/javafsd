daa924190c03dd1519063b67b2894fc64e711c21
praneeth chinnu <praneethchinna98@gmail.com>
2022-04-08 18:28:37 +0530

    Intellij

---
+++ JavaFsdProjects/src/VariableArguments.java
@@ -0,0 +1,17 @@
+public class VariableArguments
+{
+    public static void main(String[] args) {
+        display();
+        display("welcome","to","simplilearn");//three arguments
+        display(" An","example","program","varargs");//four arguments
+    }
+
+    static void display(String...values)
+{
+    System.out.println("display method invoked");
+    for (String s:values)
+    {
+        System.out.print(s);
+    }
+}
+}
